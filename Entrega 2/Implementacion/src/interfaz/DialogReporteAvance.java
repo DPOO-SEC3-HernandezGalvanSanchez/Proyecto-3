ddb1bc508c069591ecc2efd5bd85694e18d0b828
msgalvan1207 <85647232+msgalvan1207@users.noreply.github.com>
2022-06-05 22:53:52 -0500

    Reporte avance completo

--- Entrega 2/Implementacion/src/interfaz/DialogReporteAvance.java
+++ Entrega 2/Implementacion/src/interfaz/DialogReporteAvance.java
@@ -1,30 +1,67 @@
 package interfaz;
 
-import java.awt.GridLayout;
+import java.awt.FlowLayout;
+
 
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 
 import modelo.ReporteAvance;
 
+import org.knowm.xchart.*;
+
 @SuppressWarnings("serial")
-public class DialogReporteAvance extends JDialog {
+public class DialogReporteAvance extends JDialog{
 	
 	// Aqui irian atributos a utilizar
 	
+	private ReporteAvance report;
 	
 	
 	
 	
-	public DialogReporteAvance(ReporteAvance report)
+	
+	public DialogReporteAvance(ReporteAvance reporte)
 	{
-		GridLayout Grid = new GridLayout(0,2);
+		this.report = reporte;
+		
+		setLayout(new FlowLayout());
 		
 		
+		//PieChart chart = new PieChartBuilder().width(600).height(500).title("Tareas").build();
+		//chart.addSeries("Pussy", 5);
 		
+		//JPanel Chart = new XChartPanel(chart);
+		
+		this.add(TareasChart());
+		this.add(TiempoChart());
+		
+		this.setSize(1250,550);
+		this.setTitle("Reporte Avance del Proyecto");
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setModal(true);
+		setVisible(true);
 		
 	}
 	
 	
-
+	private JPanel TareasChart()
+	{
+		PieChart chart = new PieChartBuilder().width(600).height(500).title("Tareas").build();
+		chart.addSeries("Tareas terminadas", this.report.tareasTerminadas);
+		chart.addSeries("Tareas sin Terminar",this.report.totalTareas-this.report.tareasTerminadas);
+		JPanel Ret = new XChartPanel(chart);	
+		return Ret;
+	}
+	
+	
+	private JPanel TiempoChart()
+	{
+		PieChart chart = new PieChartBuilder().width(600).height(500).title("Tiempo").build();
+		chart.addSeries("Tiempo planeado terminado", this.report.tiempoPlaneadoTerminadas);
+		chart.addSeries("Tareas sin Terminar",this.report.tiempoPlaneadoTotal-this.report.tiempoPlaneadoTerminadas);
+		JPanel Ret = new XChartPanel(chart);
+		return Ret;
+	}
 }
